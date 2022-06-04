af20bdef257cbcd34e9889a28e3b5b870be075d7
Eduardo <drewobelisk@gmail.com>
2022-06-03 19:45:12 -0500

    tabla paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,14 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class PorPaisComponent implements OnInit {
 
   termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
 
   buscar(){
+    this.hayError = false;
     console.log(this.termino)
+    this.paisService.buscarPais(this.termino).subscribe(paises  =>{
+this.paises = paises;
+    },(error) =>{
+      this.hayError = true;
+      this.paises = []
+    });
   }
+
 
 }
