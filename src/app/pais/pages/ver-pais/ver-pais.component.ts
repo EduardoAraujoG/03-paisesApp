404f3e500bf6dd0988b1ce6ad905c41463c98e2d
Eduardo <drewobelisk@gmail.com>
2022-06-07 18:26:43 -0500

    creacion de ver mas para paises

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-pais.component.css']
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  pais!: Country[];
+  constructor(private activateRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(({id}) => {
+      this.paisService.getPaisporCodigo(id).subscribe(pais => {
+      this.pais = pais;
+        console.log(pais); 
+      }); 
+    });
   }
-
 }
