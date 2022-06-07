34efb0d1e7b66756896416289b09fdef59cb8f3a
Eduardo <drewobelisk@gmail.com>
2022-06-07 16:19:27 -0500

    creacion de modulos para capital

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
+import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
@@ -8,16 +9,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class PaisInputComponent implements OnInit {
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter<string>();
+  
+  debounce: Subject<string> = new Subject<string>();
+  
   termino: string = '';
-
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.debounce.pipe(
+      debounceTime(500)
+    ).subscribe(termino => this.onDebounce.emit(termino));
   }
-
   buscar(){
     //console.log(this.termino);
     this.onEnter.emit(this.termino);
   }
-
+  teclaPresionada(){
+    this.debounce.next(this.termino);
+  }
 }
--- src/app/pais/components/pais-table/pais-table.component.ts
+++ src/app/pais/components/pais-table/pais-table.component.ts
@@ -14,6 +14,7 @@ export class PaisTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
